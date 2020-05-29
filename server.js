b80f30d0e32fbacb212868752f6d2b6b88d60b8b
Rob Hess <robwhess@gmail.com>
2020-05-29 09:59:57 -0700

    Add code from 5/29 lecture.

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 var path = require('path');
 var express = require('express');
+var exphbs = require('express-handlebars');
+
+var peopleData = require('./peopleData');
+console.log("== peopleData:", peopleData);
+
 var app = express();
 
 var port = 8000;
+
+app.engine('handlebars', exphbs({ defaultLayout: null }));
+app.set('view engine', 'handlebars');
 
 app.use(express.static('public'));
 
@@ -21,9 +29,23 @@ var availablePeople = [
 app.get('/people/:person', function (req, res, next) {
   var person = req.params.person.toLowerCase();
   if (availablePeople.indexOf(person) >= 0) {
-    res.status(200).sendFile(
-      __dirname + '/public/people/' + person + '.html'
-    );
+    // res.status(200).sendFile(
+    //   __dirname + '/public/people/' + person + '.html'
+    // );
+    // res.render('photoPage', {
+    //   name: "Rey",
+    //   photos: [
+    //     {
+    //       url: "https://placekitten.com/480/480",
+    //       caption: "Rey as a kitten"
+    //     },
+    //     {
+    //       url: "https://placekitten.com/480/480",
+    //       caption: "Rey as a kitten"
+    //     }
+    //   ]
+    // });
+    res.status(200).render('photoPage', peopleData[person]);
   } else {
     next();
   }
